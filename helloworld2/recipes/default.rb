832392704981f7295b13e6b6025040c45b0c2e46
Joseph <josephccaceres@gmail.com>
2019-09-30 16:39:04 -0600

    Chef2

---
+++ helloworld2/recipes/default.rb
@@ -0,0 +1,25 @@
+#
+# Cookbook:: helloworld
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+apt_update 'Update the apt cache daily' do
+    frequency 86_400
+    action :periodic
+end
+  
+package 'apache2'
+  
+service 'apache2' do
+    supports status: true
+    action [:enable, :start]
+end
+  
+file '/var/www/html/index.html' do
+    content '<html>
+    <body>
+      <h1>hello world</h1>
+    </body>
+  </html>'
+end
